f08ce07e3ddc8a1dca807b5712a5329cfc0bde44
Satta <sattaravi@gmail.com>
2017-08-14 17:28:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 curl -o /home/soln.zip $TEMPLATE_URL && unzip /home/soln.zip -d /home/
 curl -o /home/bmcs_api_key.pem $PRIVATE_KEY_URL
-terraform init && terraform plan && terraform apply
+pwd
+terraform init 
+terraform plan
+terraform apply
