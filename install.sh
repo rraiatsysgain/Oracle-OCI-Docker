956b7c75da4f9af5b02ef51a38bfb15735716503
rraiatsysgain <rrai@sysgain.com>
2017-11-01 16:38:47 -0700

    New changes to work with azure

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget $TEMPLATE_URL -O soln.zip
-wget $PRIVATE_KEY_URL -O bmcs_api_key.pem
-v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
-v="$v."
-cp -r $v .
+
+unzip soln.zip
+#wget $PRIVATE_KEY_URL -O bmcs_api_key.pem
+#v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
+#v="$v."
+#cp -r $v .
