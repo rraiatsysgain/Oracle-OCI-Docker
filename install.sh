f52f1dfc845b944125bd343c400817114d73976d
rraiatsysgain <rrai@sysgain.com>
2017-11-01 14:08:51 -0700

    Fixed unzip logic

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -o soln.zip $TEMPLATE_URL
-wget -o bmcs_api_key.pem $PRIVATE_KEY_URL
+wget $TEMPLATE_URL -O soln.zip
+wget $PRIVATE_KEY_URL -O bmcs_api_key.pem
 v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
 v="$v."
 cp -r $v .
