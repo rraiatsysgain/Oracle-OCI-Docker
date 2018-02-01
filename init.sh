874e8290cd8fe8255db446013c84b312767f01ea
rraiatsysgain <rrai@sysgain.com>
2018-02-01 09:38:57 -0800

    updated login

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-app_secret='cat app_key.pem'
-az login --service-principal -u $AZ_VAR_app_id --password $app_secret --tenant $AZ_VAR_tenant_id
+
+az login --service-principal -u $AZ_VAR_app_id --password "$(cat app_key.pem)" --tenant $AZ_VAR_tenant_id
