b59285992378c413fa33464f5e07daa18a4ae2d4
rraiatsysgain <rrai@sysgain.com>
2018-02-01 06:08:32 -0800

    updated key

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 wget $TEMPLATE_URL -O soln.zip
 
 unzip soln.zip
-#wget $PRIVATE_KEY_URL -O bmcs_api_key.pem
+wget $PRIVATE_KEY_URL -O app_key.pem
 #v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
 #v="$v."
 #cp -r $v .
